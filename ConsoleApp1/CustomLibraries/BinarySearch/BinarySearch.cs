012b4a60961fda69ec794ff19c2124281085d881
alexratul <alexratul12@outlook.com>
2018-04-14 10:23:20 -0600

    Creating the main function of the binary search. Pending the search specific method.

--- ConsoleApp1/CustomLibraries/BinarySearch/BinarySearch.cs
+++ ConsoleApp1/CustomLibraries/BinarySearch/BinarySearch.cs
@@ -17,8 +17,17 @@ namespace ConsoleApp1.CustomLibraries.BinarySearch
                 /*get the string array with the inputed array by the user*/
                 String[] array = (Console.ReadLine()).Split(' ');
 
+                /*Read the number of queries that are going to be executed*/
+                int querySize = Int32.Parse(Console.ReadLine());
+
+                for (int i = 0; i < querySize; i++)
+                {
+                    /*call the function that is going to deliver his position on the vector*/
+                    int low  = 0;
+                    int high = size - 1;
 
 
+                }
 
             }
             catch (Exception e)
