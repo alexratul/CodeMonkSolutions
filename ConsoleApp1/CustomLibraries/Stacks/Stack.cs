d05ca4adecac3fb65da29f9446b5fcb6c3e94902
alexratul <alexratul12@outlook.com>
2018-04-15 09:11:05 -0600

    Save progress of stacks and queries (see if a make the dynamic data structure).

---
+++ ConsoleApp1/CustomLibraries/Stacks/Stack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.CustomLibraries.Stacks
+{
+    class Stack
+    {
+        public void monkLovingFood()
+        {
+            try
+            {
+                int queries = Int32.Parse(Console.ReadLine());
+
+                for (int i = 0; i < queries; i++)
+                {
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
